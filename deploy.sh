cb44be72b435f102e15fdb6d3c68e96b4e23176f
Dema-koder <asotruzikovic@gmail.com>
2024-05-28 02:40:27 +0300

    deploy.sh change

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,7 @@ unzip -o target/myapp.zip -d $DEPLOY_DIR
 
 sudo sed -i '/Environment="BOT_TOKEN=/d' /etc/systemd/system/telegram-simple-bot.service
 
-{
-    grep -v "^\[Service\]" /etc/systemd/system/telegram-simple-bot.service
-    echo "[Service]"
-    echo "Environment=\"BOT_TOKEN=${BOT_TOKEN}\""
-} | sudo tee /etc/systemd/system/telegram-simple-bot.service.tmp > /dev/null
-
-sudo mv /etc/systemd/system/telegram-simple-bot.service.tmp /etc/systemd/system/telegram-simple-bot.service
+echo "Environment=\"BOT_TOKEN=${BOT_TOKEN}\""
 
 sudo systemctl daemon-reload
 
