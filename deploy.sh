f2dd9b56fcd94200c7f737f1164a8c66f8b482bf
Dema-koder <asotruzikovic@gmail.com>
2024-05-28 01:17:21 +0300

    github-actions.yml and deploy.sh change

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,8 @@ sudo systemctl stop telegram-simple-bot
 
 unzip target/myapp.zip -d $DEPLOY_DIR
 
-sudo systemctl start telegram-simple-bot
+sudo sed -i '/\[Service\]/a Environment="BOT_TOKEN=${BOT_TOKEN}"' /etc/systemd/system/telegram-simple-bot.service
 
+sudo systemctl daemon-reload
+
+sudo systemctl start telegram-simple-bot
