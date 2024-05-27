8736221e78437341add3f7d7d392b78dc4efb730
Dema-koder <74598261+Dema-koder@users.noreply.github.com>
2024-05-28 00:43:09 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ DEPLOY_DIR="/root"
 
 cd $DEPLOY_DIR
 
-sudo systemctl stop simple-telegram-bot
+sudo systemctl stop telegram-simple-bot
 
 unzip target/myapp.zip -d $DEPLOY_DIR
 
-sudo systemctl start simple-telegram-bot
+sudo systemctl start telegram-simple-bot
 
